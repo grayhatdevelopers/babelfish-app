90030a2ada080925ddd6e5280e4bd15c0f405df3
aarijimam <35100854+aarijimam@users.noreply.github.com>
2025-05-03 19:29:48 +0500

    fix: config update

--- lib/models/api_response.dart
+++ lib/models/api_response.dart
@@ -1,10 +1,19 @@
+import 'package:audio_recorder/services/storage_service.dart';
+
 class OmniAPI {
-  static String baseUrl = 'http://192.168.1.102:8081';
+  static String baseUrl = 'https://dafa-203-82-54-66.ngrok-free.app';
   final Map<String, String> headers;
 
   OmniAPI({
     this.headers = const {'Content-Type': 'application/json'},
   });
+
+  static Future<void> initializeBaseUrl() async {
+    final savedUrl = await StorageService.getApiUrl();
+    if (savedUrl != null && savedUrl.isNotEmpty) {
+      baseUrl = savedUrl;
+    }
+  }
 
   static Uri get voiceCloneEndpoint => Uri.parse('$baseUrl/voice-clone/');
   static Uri get signupEndpoint => Uri.parse('$baseUrl/auth/signup');
